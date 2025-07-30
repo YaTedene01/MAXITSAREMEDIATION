9e125e129bb14e5dd027ab4466d255a9cfeb791d
YaTedene01 <yate0798@gmail.com>
2025-07-30 09:25:38 +0000

    the commit

--- public/index.php
+++ public/index.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_error', 1);
-ini_set('display_startup_error',1);
-error_reporting(E_ALL);
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // Load environment variables
